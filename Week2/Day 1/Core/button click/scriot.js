1bb040504f00901c2e20210a110e114a153ff798
montassar-git <montassarmoussa2000@gmail.com>
2023-05-11 20:21:59 +0100

    button clicker

---
+++ Week2/Day 1/Core/button click/scriot.js
@@ -0,0 +1,15 @@
+var aa= document.querySelector(".btn")
+function example() {
+ 
+         aa.innerText = "logout";
+    }
+    var bb= document.querySelector(".sea")
+    function rem(){
+        bb.remove();
+    }
+   
+    function aloret()
+        {
+       alert("ninja was liked");
+    }
+
