ddfc747fcaa8ea1323062ffad5fa501d0b05c442
montassar-git <montassarmoussa2000@gmail.com>
2023-05-17 19:17:39 +0100

    loop

---
+++ python/week1/forloop.py
@@ -0,0 +1,19 @@
+for i in range(1, 101):
+    if i % 5 == 0:
+      print('coding')
+    elif i % 10 == 0 :
+      print('coding dojo')
+    else :
+      print(i)
+
+total = 0
+for i  in range (1 , 5001,2):
+    total +=i
+    print(total)
+
+
+for i  in range (2018,1,-4):
+
+      print(i)
+
+      
