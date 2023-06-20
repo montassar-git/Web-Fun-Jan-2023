1e1f58f587631ff3e5e1d6bf434ad6ae71cacad9
montassar-git <montassarmoussa2000@gmail.com>
2023-06-20 18:16:51 +0100

    server dojo

---
+++ MERN/script.js
@@ -0,0 +1,22 @@
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(employee.name);
+// console.log(otherName);
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
---
+++ MERN/test.js
@@ -0,0 +1,15 @@
+const nums1 = [5,3,4,2,1];
+const nums2 = [9,2,5,6,4,3,7,10,1,8];
+
+const nums3 = [10,9,8,7,6,5,4,3,2,1];
+
+function bubblesort(nums){
+    while (i in range length.nums1)
+        
+}
+
+
+console.log(nums1);
+bubblesort(nums1);
+console.log(nums1)
+
