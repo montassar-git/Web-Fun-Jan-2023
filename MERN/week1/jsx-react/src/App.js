8be0c230c6c4578765382778a0d7107be80eeebb
montassar-git <montassarmoussa2000@gmail.com>
2023-07-04 20:55:07 +0100

    jsx

---
+++ MERN/week1/jsx-react/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello Dojo!</h1>
+      <h4>Things i need to do:</h4>
+      {/* <li>
+        <ul>
+          learn React
+        </ul>
+        <ul>
+          climb Mt.Everest
+        </ul>
+        <ul>
+          Run a marathon
+        </ul>
+        Feed the dogs
+      </li> */}
+      <li>learn React</li>
+      <li>climb Mt.Everest</li>
+      <li>Run a marathon</li>
+      <li> Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
---
+++ algo/.js
@@ -0,0 +1,11 @@
+const K=16;
+const arr[2,5,3,6,7,0,0,23,11];
+function consecutiveSums() {
+    var arr1 = [];
+    for (i=0;i< arr.length;i++){
+        if (arr[i] < k) {
+            arr1.push(arr[i]);
+          }
+        }
+        return arr1;
+      }
