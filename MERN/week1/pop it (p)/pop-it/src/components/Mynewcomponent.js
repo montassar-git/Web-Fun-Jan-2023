c46b2d5ddf9da41592eb3634f1c3645aa8c94e8b
montassar-git <montassarmoussa2000@gmail.com>
2023-07-10 16:49:50 +0100

    Putting it Together

--- MERN/week1/pop it (p)/pop-it/src/components/Mynewcomponent.js
+++ MERN/week1/pop it (p)/pop-it/src/components/Mynewcomponent.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-
-class Mynewcomponent extends Component{
-    render(){
-        return(
-            // <div>
-            //     {this.props.someText}
-            // </div>
-
-            <div className={classes.card}>
-                <div className={classes.content}>
-                    <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                    <p>Age: {this.props.age}</p>
-                    <p>Hair Color: {this.props.hairColor}</p>
-                </div>
-            </div>
-        );
-
+    
+    
+class PersonCard extends Component {
+    render() {
+        return <div>
+            <h1>{this.props.firstName}, {this.props.lastName}</h1>
+            <p>Age : {this.props.age}</p>
+            <p>Hair Color : {this.props.hairColor}</p>
+        </div>;
     }
 }
-
-export default Mynewcomponent;
+    
+export default PersonCard;
